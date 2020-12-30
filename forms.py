62d5f24bac2d5159b88c781ebc0ab8d34dbd435a
lou <louamaya@me.com>
2020-12-30 03:09:40 -0800

    create form for user feedback

--- forms.py
+++ forms.py
@@ -1,16 +1,46 @@
 from flask_wtf import FlaskForm
-from wtforms_alchemy import model_form_factory
+from wtforms import StringField, TextField, PasswordField, TextAreaField
+from wtforms.validators import InputRequired, Length, Email
 from models import db, User
 
-BaseModelForm = model_form_factory(FlaskForm)
+
+class RegistrationForm(FlaskForm):
+    """Defines form to be used for registration"""
+    username = StringField(
+        "Username",
+        validators=[InputRequired(), Length(min=1, max=20)])
+
+    password = PasswordField(
+        "Password",
+        validators=[InputRequired(), Length(min=6, max=55)])
+    email = StringField(
+        "Email",
+        validators=[InputRequired(), Email(), Length(max=50)])
+    first_name = StringField(
+        "First Name",
+        validators=[InputRequired(), Length(min=1, max=30)])
+    last_name = StringField(
+        "Last Name",
+        validators=[InputRequired(), Length(min=1, max=30)])
 
 
-class ModelForm(BaseModelForm):
-    @classmethod
-    def get_session(self):
-        return db.get_session
+class LoginForm(FlaskForm):
+    """Defines form to be used for login"""
+    username = StringField(
+        "Username",
+        validators=[InputRequired(), Length(min=1, max=20)])
+
+    password = PasswordField(
+        "Password",
+        validators=[InputRequired(), Length(min=6, max=55)])
 
 
-class UserForm(ModelForm):
-    class Meta:
-        model = User
+class FeedbackForm(FlaskForm):
+    """Defines feedback form"""
+    title = StringField(
+        "Title",
+        validators=[InputRequired(), Length(min=1, max=100)])
+
+    content = TextAreaField(
+        "Content",
+        validators=[InputRequired()])
