02e7560300c7752952dc8c602caa1155a1950564
lou <louamaya@me.com>
2020-12-30 03:10:00 -0800

    create a new table model for feedback

--- models.py
+++ models.py
@@ -1,6 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from sqlalchemy.exc import IntegrityError
 
 db = SQLAlchemy()
+bcrypt = Bcrypt()
 
 
 class User(db.Model):
@@ -13,7 +16,8 @@ class User(db.Model):
                    autoincrement=True)
 
     username = db.Column(db.String(20),
-                         nullabale=False)
+                         nullable=False,
+                         unique=True)
 
     password = db.Column(db.Text,
                          nullable=False)
@@ -24,6 +28,55 @@ class User(db.Model):
                            nullable=False)
     last_name = db.Column(db.String(30),
                           nullable=False)
+
+    feedback = db.relationship("Feedback", backref="user",
+                               cascade="all, delete-orphan")
+
+    @classmethod
+    def register(cls, username, password, email, first_name, last_name, form):
+        """Register user with hashed password and return class user"""
+
+        hashed = bcrypt.generate_password_hash(password)
+
+        # turn byte string to normal (unicode utf8) string
+        hashed_utf8 = hashed.decode("utf8")
+
+        user = cls(
+            username=username,
+            password=hashed_utf8,
+            email=email,
+            first_name=first_name,
+            last_name=last_name
+        )
+        db.session.add(user)
+        db.session.commit()
+        return user
+
+    @classmethod
+    def login(cls, username, password):
+        """Handles User Login"""
+
+        user = User.query.filter_by(username=username).first()
+        if user and bcrypt.check_password_hash(user.password, password):
+            return user
+        return False
+
+
+class Feedback(db.Model):
+    """Create feedback model"""
+
+    __tablename__ = "feedbacks"
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    title = db.Column(db.String(100),
+                      nullable=False)
+    content = db.Column(db.Text,
+                        nullable=False)
+    username = db.Column(db.String(20),
+                         db.ForeignKey('users.username'),
+                         nullable=False)
 
 
 def connect_db(app):
